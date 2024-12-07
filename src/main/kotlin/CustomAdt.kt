0abf0334b579ba066efa97971afbd7644dfd5e05
Алексей Жмылев <azhmylev@seven.tech>
2024-12-07 23:20:09 +0300

    parse int error handling impl

--- src/main/kotlin/CustomAdt.kt
+++ src/main/kotlin/CustomAdt.kt
@@ -9,7 +9,18 @@ sealed interface ParseResult {
 object CustomAdt {
 
     fun parseInt(str: String): ParseResult {
-        TODO()
+        var res = 0
+        val sign = if (str.startsWith("-")) -1 else 1
+        val symbolsToSkip = if (sign == -1) 1 else 0
+
+        for ((i, char) in str.withIndex().drop(symbolsToSkip)) {
+            if (char !in '0'..'9')
+                return ParseResult.Failure(i, char)
+
+            res = res * 10 + (char - '0')
+        }
+
+        return ParseResult.Success(res * sign)
     }
 
 }
